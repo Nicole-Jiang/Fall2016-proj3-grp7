948b90535340edddafc61ad66715fac0ef36de7c
ruby517 <jw3323@columbia.edu>
2016-10-31 19:52:46 -0400

    Update test.R

--- lib/test.R
+++ lib/test.R
@@ -2,11 +2,8 @@
 ### Fit the classification model with testing data ###
 ######################################################
 
-### Author: Yuting Ma
-### Project 3
-### ADS Spring 2016
 
-test <- function(fit_train, dat_test){
+test <- function(fit_train, data.test){
   
   ### Fit the classfication model with testing data
   
@@ -16,11 +13,53 @@ test <- function(fit_train, dat_test){
   ### Output: training model specification
   
   ### load libraries
-  library("gbm")
+  library(data.table)
+  library(EBImage)
+  library(dplyr)
+  library(xgboost)
+  library(MASS)
+  library(ada)
+  library(gbm)
+  library(randomForest)
+  library(tree)
+  library(e1071)
+  library(pROC)
+  library(class)
   
-  pred <- predict(fit_train$fit, newdata=dat_test, 
-                  n.trees=fit_train$iter, type="response")
   
+  predict_results = vector()
+  
+  ##ada
+  ada.predict <- predict(train$fit.ada,newdata=data.test,type="vector")
+  predict_results[1]= mean(data.test$filelabel==ada.predict)
+  
+  ##randeom forest
+  out.rf <- predict(gbm.fit, newdata=data.test,n.trees= train$ntrees)
+  rf.predict=as.numeric(out.rf > mean(out.rf)) 
+  predict_results[2]= mean(data.test$filelabel==rf.predict)
+  
+  ##svm
+  svm.pred= predict(svm.fit,newdata = data.test)
+  predict_results[3]=mean(data.test$filelabel==svm.pred)
+  
+  ##knn
+  knn.pred= knn(data.train[,-801], data.test[,-801], cl= data.train$filelabel, k = train$k)
+  predict_results[4]=mean(data.test$filelabel==knn.pred)
+  
+  ##xgboost
+  xg.pred <- predict(xg.fit, as.matrix(data.test[,1:800]))
+  xg.pred <- as.numeric(xg.pred > mean(xg.pred))
+  predict_results[5] <- mean(data.test$filelabel == xg.pred)
+  
+  ##majority vote
+  results = data.frame(ada=as.numeric(ada.predict)-1, 
+                       gbm=gbm.predict,rf= rf.predict,
+                       svm=as.numeric(svm.pred)-1,
+                       knn=as.numeric(knn.pred)-1)
+  
+  test= as.numeric(rowMeans(results)>0.5)
+  mean(data.test$filelabel==test)
   return(as.numeric(pred> 0.5))
+  
 }
 
