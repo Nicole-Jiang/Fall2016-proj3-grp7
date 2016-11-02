d55a25ffacc7df9185f8ac482a1e468012257fd2
jingmu2014 <vincent_muj@hotmail.com>
2016-11-02 01:30:15 -0400

    Update Final Train.R

--- lib/Final Train.R
+++ lib/Final Train.R
@@ -13,8 +13,8 @@ label= c(rep(0,1000),rep(1,1000)) #when reproducing, labels should be of numeric
 label_train=label[index]
 
 train <- function(dat_train, label_train, par=NULL){
-  dat_train1= dat_train[,1:512]
-  dat_train2= dat_train[,513:5512]
+  dat_train1= dat_train[,1:800]
+  dat_train2= dat_train[,801:5800]
   ### Train a Gradient Boosting Model (GBM) using processed features from training images
   
   ### Input: 
@@ -55,6 +55,7 @@ train <- function(dat_train, label_train, par=NULL){
       cvError.temp=cvError.temp+(1- mean(label_train[index == j]==knn.temp))/5
     }
     knn.Tuning$cvError[i]= cvError.temp
+    print(paste(i, 'done'))
   }
   ###########################   Get k for Knn model
   knn.Tuning<-knn.Tuning[order(knn.Tuning$cvError),]
@@ -105,19 +106,23 @@ train <- function(dat_train, label_train, par=NULL){
                          bag.fraction = 0.5, tuneGrid = gbmGrid)
   gbm_fit <- gbm.fit(x = dat_train1, y = label_train, n.trees = gbmfit$bestTune$n.trees, interaction.depth = gbmfit$bestTune$interaction.depth,
                      shrinkage = gbmfit$bestTune$shrinkage, n.minobsinnode = gbmfit$bestTune$n.minobsinnode, distribution = 'bernoulli')   
-  print('gbm for rgb done')
   
-  ############ GBM
-  gbmGrid2 <- expand.grid(interaction.depth = (3:5)*2 ,n.trees = (8:10)*25,shrinkage = .1,
-                          n.minobsinnode = 10)
-  gbmcontrol2 <- trainControl(method = 'cv', number = 5)
-  gbmfit2 <- caret::train(dat_train2, label_train,
-                          method = "gbm", trControl = gbmcontrol2, verbose = FALSE,
-                          bag.fraction = 0.5, tuneGrid = gbmGrid2)
-  gbm_fit2 <- gbm.fit(x = dat_train2, y = label_train, n.trees = gbmfit3$bestTune$n.trees, interaction.depth = gbmfit3$bestTune$interaction.depth,
-                      shrinkage = gbmfit3$bestTune$shrinkage, n.minobsinnode = gbmfit3$bestTune$n.minobsinnode)   
-  print('gbm for sift done')
   
+  ############This is the baseline GBM, we use the same method of tuning parameters as above
+  # gbmGrid2 <- expand.grid(interaction.depth = (3:5)*2 ,n.trees = (8:10)*25,shrinkage = .1,
+  #                         n.minobsinnode = 10)
+  # gbmcontrol2 <- trainControl(method = 'cv', number = 5)
+  # gbmfit2 <- caret::train(dat_train2, label_train,
+  #                         method = "gbm", trControl = gbmcontrol2, verbose = FALSE,
+  #                         bag.fraction = 0.5, tuneGrid = gbmGrid2)
+  # gbm_fit2 <- gbm.fit(x = dat_train2, y = label_train, n.trees = gbmfit2$bestTune$n.trees, interaction.depth = gbmfit2$bestTune$interaction.depth,
+  #                     shrinkage = gbmfit2$bestTune$shrinkage, n.minobsinnode = gbmfit2$bestTune$n.minobsinnode)
+  
+  ####the tuning takes a while to run, we find out the best n.trees = 250 and best depth = 6
+  gbm_fit2 <- gbm.fit(x = dat_train2, y = label_train, n.trees = 250,
+                      interaction.depth = 6,
+                      shrinkage = 0.1,
+                      n.minobsinnode = 10)
   
   return(list(fit_ada=ada.fit,fit_rf=rf.fit, #fit_svm= svm.fit, kernel= kernel,
               dat_train= dat_train1, label_train= label_train, k=knn.Tuning$k[1], fit_xgboost=xg.fit,
